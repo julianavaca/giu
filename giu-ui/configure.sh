147cdd1c0000016ec7a9012e29f7be7f9e34ac3a
Martin Britez <martin.britez@redb.ee>
2016-07-14 10:39:42 -0300

    add Angular 1.5.7

--- giu-ui/configure.sh
+++ giu-ui/configure.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 git config url."https://".insteadOf git:// && jspm registry create bower jspm-bower-endpoint -y && jspm config registries.npm-redbee.remote https://npm.jspm.io && jspm config registries.npm-redbee.registry http://argalephdockerd2.turner.com:8081/content/repositories/npm-redbee/ && jspm config registries.npm-redbee.handler jspm-npm && jspm install
